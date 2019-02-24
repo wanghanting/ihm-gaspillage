3bc2e5c623dd9f6d368805fe2463ae07628633ed
wanghanting <hanting.wang@etu.unice.fr>
2019-02-24 15:51:51 +0100

    ajoute aide.fxml

--- src/Program/Main.java
+++ src/Program/Main.java
@@ -10,9 +10,9 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello world");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        Parent root = FXMLLoader.load(getClass().getResource("Resources/aide.fxml"));
+        primaryStage.setTitle("Gaspillage");
+        primaryStage.setScene(new Scene(root, 650, 475));
         primaryStage.show();
     }
 

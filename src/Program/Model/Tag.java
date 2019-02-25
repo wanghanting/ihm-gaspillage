03848caa64a89f823deda594c3ae4811783fae5a
Jiazhen Liu <jiazhen.liu@etu.unice.fr>
2019-02-25 23:35:21 +0800

    add a class of tag

---
+++ src/Program/Model/Tag.java
@@ -0,0 +1,11 @@
+package Program.Model;
+
+public class Tag {
+    private String tagName;
+
+    public Tag(String name){this.tagName=name;}
+
+    public void setName(String name){this.tagName=name;}
+
+    public String getName(String name){return  this.tagName;}
+}

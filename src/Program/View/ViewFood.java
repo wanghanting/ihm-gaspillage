da64bec33898ce3e66d32c2391861f2c2b773666
Jiazhen Liu <jiazhen.liu@etu.unice.fr>
2019-02-28 04:04:34 +0800

    add food

---
+++ src/Program/View/ViewFood.java
@@ -0,0 +1,89 @@
+package Program.View;
+import Program.Model.ModelFood;
+import Program.Model.ModelListOfFood;
+import Program.Controller.ControllerFood;
+import javafx.beans.value.ChangeListener;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.control.ListCell;
+import javafx.scene.control.ListView;
+import javafx.util.Callback;
+
+import java.io.IOException;
+
+public class ViewFood {
+    static final String XML_FILE = "../Resources/page_d'accueil.fxml";
+    static final String LABEL = "HFV";
+    static final int WIDTH = 200;
+    static final int HEIGHT = 10;
+    private static final String FOOD = "../Resources/food.fxml";
+    private static int rangeSelectedItem = -1;
+    private static ModelListOfFood model;
+    private static ControllerFood controller;
+
+    public static int getRangeSelectedItem() {
+        return rangeSelectedItem;
+    }
+
+    void init(ModelListOfFood model, ControllerFood controller) {
+        ViewFood.model = model;
+        ViewFood.controller = controller;
+        //init the ObservableList of custumers to the ListView
+        controller.getFoodListView().setItems(model.getListOfFood());
+
+        //call a cell factory and display each observable item in the ListView
+        adaptItems( controller.getFoodListView() );
+
+        //listner if user click in the ListView update rangeSelectedItem value
+        listenTo( controller.getFoodListView() );
+    }
+    private void adaptItems(ListView listView) {
+        //Set a new cell factory to use in the ListView.
+        listView.setCellFactory(
+                //  first parameter specifies the type of the object passed in to the call method
+                //  the second parameter specifying the return type of the method.
+                new Callback<ListView, ListCell>() {
+                    //define what to do when ModelListCustomers.listOfCustumers is changed
+                    @Override
+                    public ListCell call(ListView listView) {
+                        return new ListCell() {
+                            @Override
+                            protected void updateItem(Object item, boolean empty) {
+                                super.updateItem(item, empty);
+                                if (item != null) {
+                                    Parent listElement=null;
+                                    // Load fxml file for this Person
+                                    FXMLLoader loader = new FXMLLoader(getClass().getResource(FOOD));
+                                    //create the person controller
+                                    ControllerFood foodController = new ControllerFood();
+                                    //attach the person controller to this person
+                                    loader.setController(foodController);
+                                    try {
+                                        listElement = loader.load(getClass().getResourceAsStream(FOOD));
+                                    } catch (IOException e) {
+                                        e.printStackTrace();
+                                    }
+                                    //initialize the person controller
+                                    foodController.init((ModelListOfFood) item);
+                                    // Display content of the fxml file
+                                    setGraphic(listElement);
+                                    adaptItems(listView);
+
+                                }
+                            }
+
+                        };
+                    }
+                });
+    }
+
+
+    private void listenTo(ListView listView) {
+        listView.getSelectionModel().selectedItemProperty().addListener(
+                (ChangeListener<ModelFood>) (observable, oldValue, newValue) -> {
+                    rangeSelectedItem = model.getListOfFood().indexOf(newValue);
+                    // --> GRRR! in javaFX the field Name is kbown in the Controller class (not in the view)
+                    //controller.getChoosenName().setText(newValue.getName());
+                });
+    }
+}

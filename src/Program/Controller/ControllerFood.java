3693449287a74ff296ee5b6dbfcb8f0e62ed138f
Jiazhen Liu <jiazhen.liu@etu.unice.fr>
2019-02-27 23:49:31 +0800

    add the list of food

--- src/Program/Controller/ControllerFood.java
+++ src/Program/Controller/ControllerFood.java
@@ -1,4 +1,11 @@
 package Program.Controller;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 
 public class ControllerFood {
+    @FXML
+    private Button btn_changerimg;
+    @FXML
+    private Button btn_ajouter;
+
 }

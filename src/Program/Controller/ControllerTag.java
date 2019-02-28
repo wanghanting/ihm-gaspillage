b7893d5931fcd1f8c6490142e723543361c26ab5
Shuangshuang Chen <chenss961214@gmail.com>
2019-02-28 15:54:40 +0100

    changer le controleur de tag.

--- src/Program/Controller/ControllerTag.java
+++ src/Program/Controller/ControllerTag.java
@@ -2,13 +2,22 @@ package Program.Controller;
 
 import Program.Model.ModelTag;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 
-import java.awt.*;
 
 public class ControllerTag {
 
     @FXML
     private Label tagName;
 
-    public void init(ModelTag tag){ tagName.setText(tag.getName());}
+    @FXML
+    public void init(ModelTag tag) {
+        try {
+            tagName.setText(tag.getName());
+        } catch (NullPointerException e) {
+            System.out.println("发生异常的是:" + e.getMessage());
+        }
+    }
+
+
 }
